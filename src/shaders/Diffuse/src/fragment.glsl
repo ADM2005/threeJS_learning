696773181a88e9ec1a78ef5885ae0f2bb89ba1cf
Adam <adammarvin05@gmail.com>
2025-02-15 06:52:43 +0400

    Updated fragment.glsl of Diffuse shader to match with fragment.js

--- src/shaders/Diffuse/src/fragment.glsl
+++ src/shaders/Diffuse/src/fragment.glsl
@@ -11,16 +11,15 @@ uniform struct Light {
 
 uniform int numLights;
 
-uniform struct ambient{
+uniform struct ambient_T{
     vec3 color;
     float intensity;
-}
-
+} ambient;
 
 vec3 computeLighting(vec3 N, vec3 L, vec3 color, float intensity){
     // Lambertian Diffuse
     float diffuse = max(dot(N, -L), 0.0);
-    return diffuse * lightColor * intensity;
+    return diffuse * color * intensity;
 }
 void main(){
     //gl_FragColor = texture2D(albedo, vUv);
@@ -28,8 +27,11 @@ void main(){
     vec3 albedo = texture2D(albedo, vUv).rgb;
 
     vec3 finalColor = vec3(0.0);
-    for(int i = 0; i < numLights; i++){
-        finalColor += computeLighting(normal, lights[i].direction, lights[i].color, lights[i].intensity);
+    for (int i = 0; i < 10; i++){
+        if(i < numLights){
+            finalColor += computeLighting(normal, lights[i].direction, lights[i].color, lights[i].intensity);
+        }
     }
+    finalColor += ambient.color * ambient.intensity;
     gl_FragColor = vec4(finalColor * albedo, 1.0);
 }
