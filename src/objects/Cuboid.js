a687a6702139c52c11a3f971f4bf29b78732a47a
Adam <adammarvin05@gmail.com>
2025-02-20 04:01:13 +0400

    added sliders!

--- src/objects/Cuboid.js
+++ src/objects/Cuboid.js
@@ -10,12 +10,34 @@ export class Cuboid{
 
         this.elapsed = 0
         this.theta = 0;
-        this.speed = 300000;
+        this.speed = 3000;
+        this.rotSpeed = 500000
+        this.radius = 2;
+
+        const radiusElement = document.getElementById('radius');
+
+        const speedElement = document.getElementById('speed');
+        const rotSpeedElement = document.getElementById('rot');
+
+
+        radiusElement.addEventListener("input", () => {
+            this.radius = parseFloat(radiusElement.value);
+        })
+        
+        speedElement.addEventListener("input", () => {
+            this.speed = parseFloat(speedElement.value) * 2000;
+        })
+        
+        rotSpeedElement.addEventListener("input", () => {
+            this.rotSpeed = parseFloat(rotSpeedElement.value) * 500000;
+        })
     }
 
     update(delta){
-        this.elapsed += delta * 1000;
-        this.mesh.rotation.x += 0.011 * delta * this.speed;
-        this.mesh.rotation.y+= 0.013 * delta * this.speed;
+        this.elapsed += delta * this.speed;
+        this.mesh.position.x = this.radius * Math.sin(this.elapsed);
+        this.mesh.position.y = this.radius * Math.cos(this.elapsed);
+        this.mesh.rotation.x += 0.011 * delta * this.rotSpeed;
+        this.mesh.rotation.y+= 0.013 * delta * this.rotSpeed;
     }
 }
